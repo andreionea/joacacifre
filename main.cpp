cd2ba8b44307137f0abfc43a9b4f3b479c6d441c
andreionea <andreionea25@outlook.com>
2017-12-05 16:56:08 +0200

    Am sters niste linii inutile, in rest nu vad o problema
    revin dupa ce compilez

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 /*Să se genereze toate numerele naturale de 3 cifre
 pentru care cifra sutelor este egală cu suma cifrelor zecilor şi unităţilor. */
 #include <iostream>
-#include <math.h>
 using namespace std;
 int main(){
     int uc, uc2;
@@ -10,12 +9,11 @@ int main(){
     for(x=100;x<=999;x++){
         aux=x;
         uc=x%10;
-        x=x/10;
-        x=floor(x);
+        x=x/10; //din moment ce x este int, operatorul div ('/') noul x va fi tot nr. intreg.
+                //div returneaza CATUL impartirii ex: int a = 3 / 2; (a=1) VS. float a = 3 / 2; (a=1.5)
         uc2=x%10;
         sumcif=uc2+uc;
         x=x/10;
-        x=floor(x);
         if(x==sumcif)
             cout<<aux<<" ";
     }
